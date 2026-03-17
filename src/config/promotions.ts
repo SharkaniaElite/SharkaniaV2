14823cfdf82c78530edafbed7d8da35d0af84adb
Sharkania Elite <duhauandres@gmail.com>
2026-03-17 06:34:39 -0300

    nuevo banner wpt global freeroll

--- src/config/promotions.ts
+++ src/config/promotions.ts
@@ -1,9 +1,9 @@
 export const WPT_PROMO = {
-  title: "Bonus WPT Global",
-  description: "Hasta $3,348 en recompensas",
-  button: "Jugar ahora",
-  image: "https://wptpartners.ck-cdn.com/tn/serve/?cid=504301",
-  link: "https://tracking.wptpartners.com/visit/?bta=35660&nci=13401",
+  title: "$10,000 gratis cada semana en WPT Global",
+  description: "Deposita $30 y juega los Elite Freerolls del sábado y domingo.",
+  button: "Quiero mi ticket →",
+  image: "https://wptpartners.ck-cdn.com/tn/serve/?cid=504427",
+  link: "https://tracking.wptpartners.com/visit/?bta=35660&nci=13647",
 
   delay: 6000,
   scrollTrigger: 0.35,
